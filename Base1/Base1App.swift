e9a2254b19f8438cca2b679412d389bff2e90cda
Nicholas Lachapelle <Nicholas.lachapelle@outlook.com>
2026-02-09 09:22:42 -0500

    Initial Commit

---
+++ Base1/Base1App.swift
@@ -0,0 +1,17 @@
+//
+//  Base1App.swift
+//  Base1
+//
+//  Created by Nicholas Lachapelle on 2026-02-09.
+//
+
+import SwiftUI
+
+@main
+struct Base1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Base1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Base1
+//
+//  Created by Nicholas Lachapelle on 2026-02-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
